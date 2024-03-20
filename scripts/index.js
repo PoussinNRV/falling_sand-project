c8a6361b8fc15ebb8fe84b23bf22b749da9d7349
PoussinNRV <isprot69@gmail.com>
2024-03-20 11:13:22 +0100

    Ajout du canevas et de la police pour le titre

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const canevasContainer = document.getElementById("canevas");
+const canevasHeight = canevasContainer.clientHeight;
+const canevasWidth = canevasContainer.clientWidth;
+
+function setup() {
+  createCanvas(canevasWidth, canevasHeight);
+}
+
+function draw() {
+  background(204);
+}
